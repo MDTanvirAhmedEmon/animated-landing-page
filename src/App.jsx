155914118a996b8f4295294db3035fd9e3b9d12e
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2024-06-24 12:45:23 +0600

    project init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./sections/Hero"
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <Hero></Hero>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,11 @@
+
+
+const Hero = () => {
+    return (
+        <div className=" bg-[#2273F8] h-[100vh]">
+
+        </div>
+    );
+};
+
+export default Hero;
